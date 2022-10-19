4a484290817bc2657ee48508b8903ce1c8f3ab6a
rPhase <25046850+rPhase@users.noreply.github.com>
2022-10-19 16:49:36 -0700

    Home, Login, Register UI

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,72 @@
 import React from 'react';
+import { useState } from 'react';
+import { FaSignInAlt } from 'react-icons/fa';
+import { toast } from 'react-toastify';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const onChangeHandler = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    toast.success('Submit');
+  };
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please log in to get support</p>
+      </section>
+      <section className='form'>
+        <form onSubmit={onSubmitHandler}>
+          {/* Email */}
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
+              value={email}
+              onChange={onChangeHandler}
+              placeholder='Enter your email'
+              required
+            />
+          </div>
+          {/* Password */}
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value={password}
+              onChange={onChangeHandler}
+              placeholder='Enter password'
+              required
+            />
+          </div>
+
+          <div className='form-group'>
+            <button className='btn btn-block'>Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default Login;
