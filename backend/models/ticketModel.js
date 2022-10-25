25cca2211de7f7600a0cb92666e890ceca931f97
rPhase <25046850+rPhase@users.noreply.github.com>
2022-10-24 17:42:57 -0700

    Ticket Model and Routes

---
+++ backend/models/ticketModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const ticketSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    product: {
+      type: String,
+      required: [true, 'Please select a product'],
+      enum: ['Xbox 360', 'Xbox One', 'Xbox Series X/S'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Please enter a description of the issue'],
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ['new', 'open', 'closed'],
+      default: 'new',
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Ticket', ticketSchema);
