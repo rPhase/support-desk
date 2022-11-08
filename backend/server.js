9ce3d35bed69af50f62db3670f9efa1bd594bfdf
rPhase <25046850+rPhase@users.noreply.github.com>
2022-11-07 18:42:12 -0800

    remove unused var

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const colors = require('colors');
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const PORT = process.env.PORT || 8000;
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.status(200).json({ message: 'Welcome to the Support Desk API' });
 });
 
@@ -23,4 +23,6 @@ app.use('/api/tickets', require('./routes/ticketRoutes'));
 // Middleware
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(colors.green.bold(`Server started on port ${PORT}`))
+);
