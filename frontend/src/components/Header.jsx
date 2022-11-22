d7575cd7c341753fd9c6a763d8d186db5b18c484
rPhase <25046850+rPhase@users.noreply.github.com>
2022-11-22 15:24:47 -0800

    Add toast for logging out

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,6 +2,7 @@ import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../features/auth/authSlice';
+import { toast } from 'react-toastify';
 
 const Header = () => {
   const navigate = useNavigate();
@@ -11,6 +12,7 @@ const Header = () => {
 
   const onLogout = () => {
     dispatch(logout());
+    toast.success('Logged out.');
     navigate('/');
   };
 
